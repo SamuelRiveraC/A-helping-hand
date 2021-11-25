d3a56ddba54b2f5285a31999ca1b26671de3dae5
samuelriverac <samuelrivera64@gmail.com>
2021-11-25 09:09:15 -0400

    hue

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://sisca.localhost/';
+$config['base_url'] = 'https://sisca-pp.herokuapp.com/';
 /*
 |--------------------------------------------------------------------------
 | Index File
