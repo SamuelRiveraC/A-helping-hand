c37f8de39d3626a4ccfeaee2be9952def7259773
samuelriverac <samuelrivera64@gmail.com>
2021-12-01 19:40:18 -0400

    do you work now please

--- application/libraries/DomP.php
+++ application/libraries/DomP.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 
-require_once APPPATH.'dompdf/autoload.inc.php';
+require_once APPPATH.'/libraries/dompdf/autoload.inc.php';
 
   use Dompdf\Dompdf;
 
